c5ad98718afc00284c9a8d0ac74e2bbd64669dd4
Aleksandra-Lung <110099673+Aleksandra-Lung@users.noreply.github.com>
2022-07-27 14:17:46 +0200

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,20 @@
 const { Result } = require("postcss");
 
-exports.createPages = ({ actions: { createPage } }) => {
-    const products = require(`./data/products.json`);
-    products.forEach(product =>{
-        createPage({
-            path: `/product/${product.slug}/`,
-            component: require.resolve(`./src/templates/product.js`),
-            context: {
-                title: product.title,
-                description: product.description,
-                image: product.image,
-                price: product.price,
-            },
-        })
-    })
-}
+// exports.createPages = ({ actions: { createPage } }) => {
+//    const products = require(`./data/products.json`);
+//    products.forEach(product =>{
+//        createPage({
+//            path: `/product/${product.slug}/`,
+//            component: require.resolve(`./src/templates/product.js`),
+//            context: {
+//                title: product.title,
+//                description: product.description,
+//                image: product.image,
+//                price: product.price,
+//            },
+//        })
+//    })
+// }
 
 
 // exports.createPages = async ({ actions: {createPage}, graphql }) => {
